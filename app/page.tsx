1fb587275ea840820c75cec8751a22ddf6bb6e3d
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-02-12 18:43:01 -0500

    Basic assets setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <Room>
-      <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>
-    </Room>
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-4xl text-white">Welcome to the Figma Clone</h1>
+    </div>
   );
 }
