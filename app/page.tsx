1c9d5eee49f3109f2360842c4a6787cce507539a
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-02-12 18:13:12 -0500

    Add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { CollaborativeApp } from "./CollaborativeApp";
 import { Room } from "./Room";
 
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+      <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>
     </Room>
   );
 }
