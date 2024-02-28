2358ee93b51332405b77db2f401194c3da46015c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-02-28 14:15:28 -0500

    Implement live cursors

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LiveCursors from './cursor/LiveCursors'
 import { useCallback } from 'react'
 import { useBroadcastEvent, useEventListener, useMyPresence, useOthers } from "@/liveblocks.config";
+import CursorChat from './cursor/CursorChat';
+import { CursorMode } from '@/types/type';
 
 const Live = () => {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+    const [cursorState, setCursorState] = useState({
+         mode: CursorMode.Hidden,
+    })
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
@@ -17,7 +23,7 @@ const Live = () => {
     }, []);
 
     const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-        event.preventDefault();
+        setCursorState({ mode: CursorMode.Hidden });
 
         updateMyPresence({ cursor: null, message: null });
     }, []);
@@ -40,6 +46,16 @@ const Live = () => {
             className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
         >
             <h1 className="text-4xl text-white">Liveblocks Figma Clone</h1>
+
+            {cursor && (
+                <CursorChat
+                    cursor={cursor}
+                    cursorState={cursorState}
+                    setCursorState={setCursorState}
+                    updateMyPresence={updateMyPresence}
+                />
+            )}
+
             <LiveCursors others={others} />
         </div>
     )
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import CursorSVG from '@/public/assets/CursorSVG'
+import { CursorChatProps, CursorMode } from '@/types/type'
+import React, { useEffect } from 'react'
 
-const CursorChat = () => {
-  return (
-    <div>CursorChat</div>
-  )
+
+
+const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        updateMyPresence({ message: event.target.value });
+        setCursorState({
+            mode: CursorMode.Chat,
+            previousMessage: null,
+            message: event.target.value,
+        })
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            setCursorState({
+                mode: CursorMode.Chat,
+                previousMessage: '',
+                message: '',
+
+            })
+        } else if (event.key === 'Escape') {
+            setCursorState({
+                mode: CursorMode.Hidden,
+            })
+        }
+    }
+
+    useEffect(() => {
+        const onKeyUp = (event: KeyboardEvent) => {
+            if (event.key === '/') {
+                setCursorState({
+                    mode: CursorMode.Chat,
+                    previousMessage: null,
+                    message: '',
+                });
+            } else if (event.key === 'Escape') {
+                updateMyPresence({ message: '' });
+                setCursorState({
+                    mode: CursorMode.Hidden,
+                });
+            }
+        }
+
+        const onKeyDown = (event: KeyboardEvent) => {
+            if (event.key === '/') {
+                event.preventDefault();
+            }
+        }
+
+        window.addEventListener('keyup', onKeyUp);
+        window.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            window.removeEventListener('keyup', onKeyUp);
+            window.removeEventListener('keydown', onKeyDown);
+        }
+
+    }, [updateMyPresence]);
+
+
+    return (
+        <div className='absolute top-0 left-0' style={{ transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)` }}>
+            {cursorState.mode === CursorMode.Chat && (
+                <>
+                    <CursorSVG color='#000' />
+                    <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'>
+                        {cursorState.previousMessage && (
+                            <div className='mb-2'>{cursorState.previousMessage}</div>
+                        )}
+                        <input
+                            title='Type a message'
+                            className='z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none'
+                            autoFocus={true}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            placeholder={cursorState.previousMessage ? '' : 'Type a message'}
+                            value={cursorState.message}
+                            maxLength={50}
+                        />
+                    </div>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default CursorChat
